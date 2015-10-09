5b7cff80b94c590d615ecba6bc077770f9bf5034
Simon D. Levy <simon.d.levy@gmail.com>
2015-10-09 16:06:45 -0400

    Fixed typo

--- examples/GPGGA/GPGGA.ino
+++ examples/GPGGA/GPGGA.ino
@@ -18,7 +18,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License 
 along with this code.  If not, see <http:#www.gnu.org/licenses/>.
 */
-*/
 
 #include <NMEA.h>
 
